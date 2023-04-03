70fe40469eb584a4ae8689c687505044a4fbb611
Kamal Yadav <kamal2629@Kamals-MacBook-Air.local>
2023-04-03 11:24:27 +0530

    getting cors error

--- index.js
+++ index.js
@@ -15,6 +15,11 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
     next();
 });
+
+app.use(cors({
+    origin: true,
+    methods: ["POST", "GET", "PATCH", "DELETE", "OPTIONS"]
+}));
   
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
